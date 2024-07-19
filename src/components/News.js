92e8b15b48bc5343c79d75572232e72cbb6e7406
TheMas18 <mayureshsamant05@gmail.com>
2024-07-20 00:09:43 +0530

    Iterating Articles

--- src/components/News.js
+++ src/components/News.js
@@ -2,71 +2,73 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-    articles = [
-        {
-            "source": {
-                "id": "bbc-sport",
-                "name": "BBC Sport"
-            },
-            "author": null,
-            "title": "England vs New Zealand LIVE: Fifth women’s T20 – cricket score, radio commentary, video highlights and text updates",
-            "description": "England host New Zealand in the fifth women’s international T20 at Lord's, London – follow text updates, radio commentary and video highlights.",
-            "url": "http://www.bbc.co.uk/sport/cricket/live/cg33jge3dkvt",
-            "urlToImage": "https://static.files.bbci.co.uk/ws/simorgh-assets/public/sport/images/metadata/poster-1024x576.png",
-            "publishedAt": "2024-07-17T16:52:20.7163082Z",
-            "content": "England captain Heather Knight: \"We want to go unbeaten this summer, that's our main objective. We want to take that momentum into our next game. The support has been really good and the crowd is rea… [+368 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ];
+    // articles = [
+    //     {
+    //         "source": {
+    //             "id": "bbc-sport",
+    //             "name": "BBC Sport"
+    //         },
+    //         "author": null,
+    //         "title": "England vs New Zealand LIVE: Fifth women’s T20 – cricket score, radio commentary, video highlights and text updates",
+    //         "description": "England host New Zealand in the fifth women’s international T20 at Lord's, London – follow text updates, radio commentary and video highlights.",
+    //         "url": "http://www.bbc.co.uk/sport/cricket/live/cg33jge3dkvt",
+    //         "urlToImage": "https://static.files.bbci.co.uk/ws/simorgh-assets/public/sport/images/metadata/poster-1024x576.png",
+    //         "publishedAt": "2024-07-17T16:52:20.7163082Z",
+    //         "content": "England captain Heather Knight: \"We want to go unbeaten this summer, that's our main objective. We want to take that momentum into our next game. The support has been really good and the crowd is rea… [+368 chars]"
+    //     },
+    //     {
+    //         "source": {
+    //             "id": "espn-cric-info",
+    //             "name": "ESPN Cric Info"
+    //         },
+    //         "author": null,
+    //         "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+    //         "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+    //         "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+    //         "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+    //         "publishedAt": "2020-04-27T11:41:47Z",
+    //         "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+    //     },
+    //     {
+    //         "source": {
+    //             "id": "espn-cric-info",
+    //             "name": "ESPN Cric Info"
+    //         },
+    //         "author": null,
+    //         "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+    //         "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+    //         "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+    //         "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+    //         "publishedAt": "2020-03-30T15:26:05Z",
+    //         "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    //     }
+    // ];
 
     constructor() {
         super();
         this.state = {
-            articles:this.articles ,
-            loading:false 
+            articles: [],
+            loading: false
         }
     }
 
+     async componentDidMount(){
+        let url="https://newsapi.org/v2/everything?domains=wsj.com&apiKey=76eaeb031e1643cb8962419db8b9ff6e";
+        let data= await fetch(url);
+        let parseData=await data.json();
+        this.setState({articles:parseData.articles});
+    }
 
     render() {
         return (
             <div className="container my-3">
                 <h1>News Monkey - Top Headlines</h1>
-                <div className="row">
-                    <div className="col-md-4">
-                        <NewsItem title="myTitle" description="Mydesc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"  newsUrl="todo"/>
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="myTitle" description="Mydesc" />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="myTitle" description="Mydesc" />
-                    </div>
+                <div className="row my-3">
+                    {this.state.articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title? element.title.slice(0, 45):" "} description={element.description?element.description.slice(0, 80):" "} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
+                    })}
                 </div>
             </div>
         );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export default class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://s.yimg.com/ny/api/res/1.2/Tm2tNZPHeep0w6tjK.9T8Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD02MDA-/https://media.zenfs.com/en/the_wall_street_journal_hosted_996/c9a75eb46f0f1e0f602d3d74ec0ba927" :imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-sm btn-primary">
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary" rel="noreferrer">
               Read More
             </a>
           </div>
